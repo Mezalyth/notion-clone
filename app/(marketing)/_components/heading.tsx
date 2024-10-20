c8f2ef9056f15468f690450d4666685145b278e1
Mezalyth <86799855+Mezalyth@users.noreply.github.com>
2024-10-19 23:57:02 -0700

    marketing page entry and footer component

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,11 +1,21 @@
 "use client";
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
-            <h1>
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
                 Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Potion</span>
             </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Potion is the connected workspace where <br />
+                better, faster work happens.
+            </h3>
+            <Button>
+                Enter Potion
+                <ArrowRight className="h-4 w-4 ml-2" />
+            </Button>
         </div>
     )
 }
